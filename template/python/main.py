e6d87cfbafbad0c682be56b85961914c7f07485b
Marco Yang <marcoyang123@gmail.com>
2022-12-16 23:30:15 -0500

    update python template

--- template/python/main.py
+++ template/python/main.py
@@ -7,19 +7,4 @@ def get_lines() -> List[str]:
         return [line.strip() for line in list(f)]
 
 
-def part1(lines):
-    return 0
-
-
-def part2(lines):
-    return 0
-
-
-def main() -> None:
-    lines = get_lines()
-    print(part1(lines))
-    print(part2(lines))
-
-
-if __name__ == '__main__':
-    main()
+lines = get_lines()
