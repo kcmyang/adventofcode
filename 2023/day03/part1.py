b73c23bcf987ca892e40867a4028b1acb5ee6c1c
Marco Yang <marcoyang123@gmail.com>
2023-12-03 00:30:15 -0500

    2023 day 03

---
+++ 2023/day03/part1.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def get_lines() -> list[str]:
+    with open(sys.argv[1]) as f:
+        return [line.strip() for line in list(f)]
+
+
+board = get_lines()
+m = len(board)
+n = len(board[0])
+
+seen = [[False for _ in range(n)] for _ in range(m)]
+dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+total = 0
+
+for r in range(m):
+    for c in range(n):
+        if board[r][c] not in '0123456789.':
+            # find adjacent numbers
+            for dr, dc in dirs:
+                rr, cc = r + dr, c + dc
+
+                if 0 <= rr < m and 0 <= cc < n and board[rr][cc].isdigit() and not seen[rr][cc]:
+                    left = cc
+                    right = cc
+
+                    while left - 1 >= 0 and board[rr][left - 1].isdigit():
+                        left -= 1
+
+                    while right + 1 < n and board[rr][right + 1].isdigit():
+                        right += 1
+
+                    total += int(board[rr][left:right + 1])
+
+                    for i in range(left, right + 1):
+                        seen[rr][i] = True
+
+print(total)
