7ebe79a5045bd6b776b02af3debd1c3c23dc5294
Marco Yang <kcmyang@proton.me>
2024-12-05 23:52:31 -0500

    test commit

---
+++ 2024/day06/part1.py
@@ -0,0 +1,14 @@
+import collections
+import itertools
+import math
+import re
+import sys
+
+
+def get_lines() -> list[str]:
+    filename = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+    with open(filename) as f:
+        return [line.strip() for line in f]
+
+
+lines = get_lines()
