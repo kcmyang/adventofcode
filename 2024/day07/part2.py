059e5220103b8096c263de4bc79caa15b5e31c78
Marco Yang <kcmyang@proton.me>
2024-12-07 00:21:01 -0500

    2024 day 07

---
+++ 2024/day07/part2.py
@@ -0,0 +1,36 @@
+import re
+import sys
+
+
+def get_lines() -> list[str]:
+    filename = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+    with open(filename) as f:
+        return [line.strip() for line in f]
+
+
+lines = get_lines()
+
+out = 0
+
+for l in lines:
+    vals = re.findall(r'\d+', l)
+    test = int(vals[0])
+    vals = list(map(int, vals[1:]))
+
+    bank = set()
+    bank.add(vals[0])
+
+    for v in vals[1:]:
+        bn = set()
+
+        for x in bank:
+            bn.add(x + v)
+            bn.add(x * v)
+            bn.add(x * (10**len(str(v))) + v)
+
+        bank = bn
+
+    if test in bank:
+        out += test
+
+print(out)
