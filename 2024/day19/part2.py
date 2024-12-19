51f4733337c1c6a686934aba2f6c457d669d3c5d
Marco Yang <marco.yang@getjerry.com>
2024-12-19 00:08:55 -0500

    2024 day 19

---
+++ 2024/day19/part2.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def get_lines() -> list[str]:
+    filename = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+    with open(filename) as f:
+        return [line.strip() for line in f]
+
+
+lines = get_lines()
+
+pieces = set(lines[0].split(', '))
+
+
+def check(pattern: str) -> int:
+    n = len(pattern)
+    dp = [0] * (n + 1)
+    dp[0] = 1
+
+    for i in range(n):
+        if dp[i]:
+            for p in pieces:
+                if pattern[i:i + len(p)] == p:
+                    dp[i + len(p)] += dp[i]
+
+    return dp[n]
+
+
+count = sum(check(line) for line in lines[2:])
+
+print(count)
